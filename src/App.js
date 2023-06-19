141ed05eaf82eb184d65ff4a29bcd83a74b93bd1
hamzona <hamzadjedovic2000@gmail.com>
2023-06-19 23:14:30 +0200

    a

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get("https://testb-urt9.onrender.com/get").then((res) => {
+      setItems(res.data[0].niz);
+    });
+  }, []);
+
+  function add() {
+    axios.post("https://testb-urt9.onrender.com/add").then((res) => {
+      setItems(res.data.niz);
+    });
+  }
+
+  function del() {
+    axios.delete("https://testb-urt9.onrender.com/delete").then((res) => {
+      setItems(res.data.niz);
+    });
+  }
+  return (
+    <div>
+      <button
+        onClick={() => {
+          add();
+        }}
+      >
+        ADD
+      </button>
+      <button
+        onClick={() => {
+          del();
+        }}
+      >
+        DELETE
+      </button>
+      {items.map((item, index) => {
+        return <div key={index}>{index}</div>;
+      })}
+    </div>
+  );
+}
+
+export default App;
